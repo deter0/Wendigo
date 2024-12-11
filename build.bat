fd3f72d847129a9a8f07f9dedbf8485b972d3fbd
deter0 <deter0109@gmail.com>
2024-12-10 16:26:51 -0800

    Don't use Raylib-J
    Avoid overhead

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo Building file with Java:
-
-"./jdk-18/bin/javac.exe" -version || exit /b
-"./jdk-18/bin/javac.exe" -cp ./lib/Raylib-J-0.5.2.jar ./src/*.java -d ./build || exit /b
-
-echo Build successful!
--- run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-java -cp ./build;./lib/Raylib-J-0.5.2.jar -Djava.library.path=./lib Wend || exit /b
-
-echo Program execution finished.
