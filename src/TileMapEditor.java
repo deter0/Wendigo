7eda0739cbcf37443ddc5882a0f302380d841497
deter0 <deter0109@gmail.com>
2024-12-16 10:21:40 -0500

    Introduction of panel system

--- src/TileMapEditor.java
+++ src/TileMapEditor.java
@@ -28,24 +28,26 @@ enum PanelLayoutSpacing {
 }
 
 class Panel {
-    public static Panel root;
+    public static Panel root = null;
 
     protected Panel[] children = new Panel[8];
-    protected Panel parent;
+    protected Panel parent = null;
 
-    public Color background = GG.COLOR_OPAQUE;
+    public Color background = Color.DARK_GRAY;
     public Color foreground = Color.WHITE;
 
-    public Vector2 position;
-    public Vector2 size;
+    public boolean positionAbsolute = false;
+    public Vector2 position = new Vector2();
+    public Vector2 size = new Vector2();
 
-    protected Vector2 computedPosition;
-    protected Vector2 computedSize;
+    protected Vector2 computedPosition = new Vector2();
+    protected Vector2 computedSize = new Vector2();
 
-    public double fixedSpacing;
+    public double fixedSpacing = 0.0;
 
-    public PanelLayoutDir layoutDir;
-    public PanelLayoutSpacing layoutSpacing;
+    public PanelLayoutDir layoutDir = PanelLayoutDir.NONE;
+    public PanelLayoutSpacing layoutSpacing = PanelLayoutSpacing.FIXED_SPACE;
+
 
     public Panel() {
         for (int i = 0; i < this.children.length; i++) {
@@ -54,24 +56,78 @@ class Panel {
     }
 
     public void ComputeSize() {
-        this.computedSize = this.size;
+        double pWidth = Game.WIDTH, pHeight = Game.HEIGHT;
+        if (this.parent != null && this.parent.computedSize != null) {
+            pWidth = this.parent.computedSize.x;
+            pHeight = this.parent.computedSize.y;
+        }
+
+        this.computedSize = new Vector2(this.size.x <= 1.0 ? this.size.x * pWidth : this.size.x,
+                                        this.size.y <= 1.0 ? this.size.y * pHeight : this.size.y);
     }
     
     public void ComputeLayout() {
-        double x = this.computedPosition.x;
-        for (int i = 0; i < this.children.length; i++) {
-            Panel child = this.children[i];
-            if (child != null) {
-                child.ComputeLayout();
+        this.ComputeSize();
+
+        double pX = 0, pY = 0;
+        if (this.parent != null && this.parent.computedSize != null) {
+            pX  = this.parent.computedPosition.x;
+            pY  = this.parent.computedPosition.y;
+        }
+
+        this.computedPosition = this.computedPosition.add(new Vector2(pX, pY));
+
+        if (this.positionAbsolute) {
+            this.computedPosition = this.computedPosition.add(this.position);
+        }
+
+        if (this.layoutDir == PanelLayoutDir.NONE) {
+            for (int i = 0; i < this.children.length; i++) {
+                Panel child = this.children[i];
+                if (child != null) {
+                    child.computedPosition = child.position;
+                    child.ComputeLayout();
+                }
             }
-
-
+        } else if (this.layoutDir == PanelLayoutDir.HORIZONTAL) {
+            if (this.layoutSpacing == PanelLayoutSpacing.FIXED_SPACE) {
+                Vector2 childPosition = new Vector2(0, 0);
+                for (int i = 0; i < this.children.length; i++) {
+                    Panel child = this.children[i];
+                    if (child != null) {
+                        child.ComputeSize();
+                        child.computedPosition = child.computedPosition.add(childPosition);
+                        childPosition = childPosition.add(new Vector2(child.computedSize.x + this.fixedSpacing, 0.0));
+                        System.out.println(i + ": " + childPosition);
+                        child.ComputeLayout();
+                    }
+                }
+            } else if (this.layoutSpacing == PanelLayoutSpacing.SPACE_EVENLY) {
+                double spacingLeft = this.computedSize.x, spacingPerChild;
+                int numChildren = 0;
+                for (int i = 0; i < this.children.length; i++) {
+                    Panel child = this.children[i];
+                    if (child != null) {
+                        numChildren++;
+                        child.ComputeSize();
+                        spacingLeft -= child.computedSize.x;
+                    }
+                }
+                spacingPerChild = spacingLeft/(double)numChildren;
+                // if (child != null) {
+                //     child.ComputeSize();
+                //     child.computedPosition = child.computedPosition.add(childPosition);
+                //     childPosition = childPosition.add(new Vector2(child.computedSize.x + this.fixedSpacing, 0.0));
+                //     System.out.println(i + ": " + childPosition);
+                //     child.ComputeLayout();
+                // }
+            }
         }
     }
 
     public void Draw(Graphics g) {
         g.setColor(this.background);
-        GG.drawRect(this.position.x, this.position.y, this.size.x, this.size.y);
+        GG.fillRect(this.computedPosition.x, this.computedPosition.y, this.computedSize.x, this.computedSize.y);
 
         for (int i = 0; i < this.children.length; i++) {
             if (this.children[i] != null) {
@@ -117,6 +173,9 @@ class Panel {
         Panel layout = new Panel();
         layout.layoutDir = dir;
         layout.layoutSpacing = spacing;
+        layout.size = new Vector2(1.0, 1.0);
+        layout.position = new Vector2();
+        layout.background = GG.COLOR_OPAQUE;
         layout.fixedSpacing = fixedSpacing;
         layout.SetParent(Panel.root);
         Panel.root = layout;
@@ -131,8 +190,9 @@ class Panel {
         }
     }
 
-    public static void Finish() {
+    public static void Finish(Graphics g) {
         Panel.root.ComputeLayout();
+        Panel.root.Draw(g);
     }
 }
 
@@ -171,16 +231,34 @@ public class TileMapEditor extends GameObject {
         
         DragPanel(bottomPanel);
 
-        g.setColor(Color.DARK_GRAY);
-        g.fillRect(bottomPanel.x, bottomPanel.y, bottomPanel.width, bottomPanel.height);
-        g.setColor(Color.LIGHT_GRAY);
-        g.drawRect(bottomPanel.x, bottomPanel.y, bottomPanel.width - 1, bottomPanel.height - 1);
+        Panel n = new Panel();
+        n.position = new Vector2(bottomPanel.x, bottomPanel.y);
+        n.size = new Vector2(1.0, bottomPanel.height);
+        n.positionAbsolute = true;
+        Panel.SetRoot(n);
 
-        double y = bottomPanel.y + 20.0, x = bottomPanel.x + 4.0;
+        Panel p = Panel.BeginLayout(PanelLayoutDir.HORIZONTAL, PanelLayoutSpacing.FIXED_SPACE, 20.0);
+        p.background = Color.RED;
+        for (int i = 0; i < 3; i++) {
+            p = Panel.BeginLayout(PanelLayoutDir.NONE, PanelLayoutSpacing.FIXED_SPACE, 0.0);
+            p.size = new Vector2(100, 100);
+            p.background = Color.YELLOW;
+            Panel.End();
+        }
+        Panel.End();
+        
+        Panel.Finish(g);
 
-        g.setFont(Game.font16);
-        GG.drawString("Map: " + this.currentMap.name, x, y);
-        y += Game.font16.getSize();
+        // g.setColor(Color.DARK_GRAY);
+        // g.fillRect(bottomPanel.x, bottomPanel.y, bottomPanel.width, bottomPanel.height);
+        // g.setColor(Color.LIGHT_GRAY);
+        // g.drawRect(bottomPanel.x, bottomPanel.y, bottomPanel.width - 1, bottomPanel.height - 1);
+
+        // double y = bottomPanel.y + 20.0, x = bottomPanel.x + 4.0;
+
+        // g.setFont(Game.font16);
+        // GG.drawString("Map: " + this.currentMap.name, x, y);
+        // y += Game.font16.getSize();
         // GG.drawString("Hello", x, y);
     }
 }
